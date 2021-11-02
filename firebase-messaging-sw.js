609c9ab2e2d6c050827aa1ae0806c89c25e837d5
bao-air-m1 <quocbao@vinaas.com>
2021-11-02 13:13:40 +0700

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,8 @@
-importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js')
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
 var firebaseConfig = {
   apiKey: "AIzaSyCpP1I0ELs9-4hwYzbprww_AmWXWZXSxBA",
@@ -15,11 +18,25 @@ const app = firebase.initializeApp(firebaseConfig)
 app.messaging().getToken({ vapidKey: 'BGxx-fjPINkBky_OPMHnaEt5I-Nw6T4b-tyQC9BBoXXGF3a0Romv0Y4PP2oB9heOK4pCh6EgNEXYj9yWWEAba6I' })
 .then((currentToken) => {
   if (currentToken) {
-    console.log('client token', currentToken)
-    localStorage.setItem('fcm-token', currentToken);
+    console.log('firebase-serviceworker token', currentToken)
+    // localStorage.setItem('fcm-token', currentToken);
   } else {
     console.log('No registration token available. Request permission to generate one.');
   }
 }).catch((err) => {
   console.log('An error occurred while retrieving token. ', err);
 })
+
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
